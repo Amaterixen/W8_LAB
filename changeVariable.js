b8dee045d421194ec61239bfdd7f40218ec08539
Josh Cassidy <Josh Cassidy>
2016-11-10 14:42:19 +0000

    Added programs from exercise 2

---
+++ changeVariable.js
@@ -0,0 +1,3 @@
+var morningAlarm = '6:30AM';
+var morningAlarm = '7:45AM';
+console.log('Morning alarm is set to:', morningAlarm);
---
+++ werewolf.js
@@ -0,0 +1,6 @@
+var moonPhase = 'full';
+if(moonPhase='full'){
+  console.log('Howwwwlll!!');
+}else{
+  console.log('I swear I am not a werewolf...!')
+}
